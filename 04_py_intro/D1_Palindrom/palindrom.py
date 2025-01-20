1e9073c2d6b9826125ec920f4caec8144cf39edb
1182-25-HTL3R <135468563+1182-25-HTL3R@users.noreply.github.com>
2025-01-20 12:26:08 +0100

    UE04_D1: Erstellung der is_palindrom Funktion

--- 04_py_intro/D1_Palindrom/palindrom.py
+++ 04_py_intro/D1_Palindrom/palindrom.py
@@ -1,1 +1,17 @@
-print("hello")
+"""
+Class: 4CN
+Program UE04_Python_Intro
+"""
+__author__ = "Fabian Ha"
+
+def is_palindrom (s:str) -> bool:
+    """
+    check if string is palindrom
+    :param s: string to be checked
+    :return: True -> palindrom, False -> no palindrom
+    """
+
+    return s.lower() == s[::-1].lower()
+
+if __name__ == '__main__':
+    print(is_palindrom('abcddcb'))
