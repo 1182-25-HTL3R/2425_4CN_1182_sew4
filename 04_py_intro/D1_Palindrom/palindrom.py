e945fadfd2e614473b67bcb665c3a8b2a25465f7
1182-25-HTL3R <135468563+1182-25-HTL3R@users.noreply.github.com>
2025-01-20 12:29:40 +0100

    UE04_D1: Erstellung der is_palindrom_sentence Funktion

--- 04_py_intro/D1_Palindrom/palindrom.py
+++ 04_py_intro/D1_Palindrom/palindrom.py
@@ -13,5 +13,19 @@ def is_palindrom (s:str) -> bool:
 
     return s.lower() == s[::-1].lower()
 
+def is_palindrom_sentence(s:str) -> bool:
+    """
+    check if string sentence is palindrom
+    :param s: string sentence to be checked
+    :return: True -> palindrom, False -> no palindrom
+    """
+
+    s2 = ""
+    for ch in s:
+        if ch.isalnum():
+            s2 += ch
+
+    return is_palindrom(s2)
+
 if __name__ == '__main__':
     print(is_palindrom('abcddcb'))
