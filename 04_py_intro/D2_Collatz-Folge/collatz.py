782787d368b9e501337febe00944f6475e9a9bc7
1182-25-HTL3R <135468563+1182-25-HTL3R@users.noreply.github.com>
2025-02-18 08:48:37 +0100

    UE04_D2: Verbesserung der collatz_sequence Funktion und Erstellung longest_collatz_sequence Funktion

--- 04_py_intro/D2_Collatz-Folge/collatz.py
+++ 04_py_intro/D2_Collatz-Folge/collatz.py
@@ -4,6 +4,8 @@ Program UE04_Python_Intro - Collatz
 """
 __author__ = "Fabian Ha"
 
+from typing import Tuple
+
 from selenium.webdriver.common.devtools.v85.debugger import step_out
 
 
@@ -29,16 +31,34 @@ def collatz_sequence(number: int, output: list = None) -> list[int]:
     if output is None:
         output = []
     if number == 1:
+        output.append(1)
         return output
 
-    next_value = collatz(number)
-    output.append(next_value)
-    return collatz_sequence(next_value, output)
+    output.append(number)
+    return collatz_sequence(collatz(number), output)
 
+def longest_collatz_sequence(number: int) -> Tuple[int, int]:
+    """
+    :param number: Startzahl
+    :return: Startwert und Länge der längsten Collatz Zahlenfolge deren Startwert <=n ist
+
+    >>> longest_collatz_sequence(100)
+    (97, 119)
+    """
+
+    longest_sequence = []
+    longest_sequence_number = None
+    for i in range(1, number):
+        sequence = collatz_sequence(i)
+        if longest_sequence.__len__() < sequence.__len__():
+            longest_sequence = sequence
+            longest_sequence_number = i
+
+    return longest_sequence_number, longest_sequence.__len__()
 
 if __name__ == "__main__":
     assert(collatz(11) == 34)
     assert(collatz(52) == 26)
 
-    assert (collatz_sequence(5) == [16, 8, 4, 2, 1])
-    assert (collatz_sequence(19) == [58, 29, 88, 44, 22, 11, 34, 17, 52, 26, 13, 40, 20, 10, 5, 16, 8, 4, 2, 1])
+    assert (collatz_sequence(5) == [5, 16, 8, 4, 2, 1])
+    assert (collatz_sequence(19) == [19, 58, 29, 88, 44, 22, 11, 34, 17, 52, 26, 13, 40, 20, 10, 5, 16, 8, 4, 2, 1])
