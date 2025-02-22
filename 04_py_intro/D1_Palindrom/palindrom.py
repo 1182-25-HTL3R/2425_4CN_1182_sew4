939b8215e37639aa7c768cbb0bc52c2fa9ebc2f7
1182-25-HTL3R <135468563+1182-25-HTL3R@users.noreply.github.com>
2025-02-22 11:33:26 +0100

    UE04_D1: Verbesserung der Code-Formatierung

--- 04_py_intro/D1_Palindrom/palindrom.py
+++ 04_py_intro/D1_Palindrom/palindrom.py
@@ -4,7 +4,8 @@ Program UE04_Python_Intro - Palindrom
 """
 __author__ = "Fabian Ha"
 
-def is_palindrom (s:str) -> bool:
+
+def is_palindrom(s: str) -> bool:
     """
     check if string is palindrom
     :param s: string to be checked
@@ -13,7 +14,8 @@ def is_palindrom (s:str) -> bool:
 
     return s.lower() == s[::-1].lower()
 
-def is_palindrom_sentence(s:str) -> bool:
+
+def is_palindrom_sentence(s: str) -> bool:
     """
     check if string sentence is palindrom
     :param s: string sentence to be checked
@@ -37,8 +39,8 @@ def palindrom_product(x: int) -> int:
 
     biggest_pal = 0
 
-    for a in range (999, 99, -1):
-        for b in range (999, 99, -1):
+    for a in range(999, 99, -1):
+        for b in range(999, 99, -1):
             product = a * b
             if product >= x:
                 continue
@@ -47,7 +49,8 @@ def palindrom_product(x: int) -> int:
 
     return biggest_pal
 
-def to_base (number: int, base: int) -> str:
+
+def to_base(number: int, base: int) -> str:
     """
     converts number to other base
     :param number: Zahl im 10er-System
@@ -58,15 +61,17 @@ def to_base (number: int, base: int) -> str:
     '4D2'
     """
 
-    list = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    list = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
+            'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     s = ""
 
     while number > 0:
-        x = list[number%base]
-        number = number//base
+        x = list[number % base]
+        number = number // base
         s = x + s
 
     return s
+
 
 def get_dec_hex_palindrom(x: int) -> int:
     """
@@ -75,11 +80,12 @@ def get_dec_hex_palindrom(x: int) -> int:
     :return: größte Zahl die im Dezimalsystem und im Hexadezimalsystem ein Palindrom ist
     """
 
-    for num in range(x-1, 0, -1):
+    for num in range(x - 1, 0, -1):
         if is_palindrom(str(to_base(num, 16))) and is_palindrom(str(num)):
             return num
 
     return -1
+
 
 if __name__ == '__main__':
     assert is_palindrom('fabiannaibAf') == True
