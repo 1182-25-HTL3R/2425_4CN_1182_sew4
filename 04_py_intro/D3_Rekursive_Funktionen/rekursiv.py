7fb01c130ec32c48f9461a3f47012a07daa8ece3
1182-25-HTL3R <135468563+1182-25-HTL3R@users.noreply.github.com>
2025-02-22 11:07:07 +0100

    UE04_D3: Erstellung von __main__ zum Testen der Funktion M

--- 04_py_intro/D3_Rekursive_Funktionen/rekursiv.py
+++ 04_py_intro/D3_Rekursive_Funktionen/rekursiv.py
@@ -1,5 +1,24 @@
+from time import time
+
+
 def M(n: int) -> int:
     if n <= 100:
         return M(M(n + 11))
     else:
         return n - 10
+
+if __name__ == '__main__':
+    t0 = time()
+
+    m_list = []
+    for i in range(200):
+        m_list.append(M(i))
+
+
+    m_dict = dict()
+    for i in range(200):
+        m_dict[i] = M(i)
+
+    print(str(time()-t0) + "sek")
+    print(m_list)
+    print(m_dict)
